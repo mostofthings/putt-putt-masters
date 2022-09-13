b4525c4c11860b2381e2a967569c07637ef58b71
roblouie <rob.j.louie@gmail.com>
2022-09-13 14:45:05 -0500

    Make performance fast

--- src/index.ts
+++ src/index.ts
@@ -18,24 +18,34 @@ import { controls } from '@/core/controls';
 import { createGameStateMachine, getGameStateMachine } from '@/game-state-machine';
 import { gameState } from '@/game-states/game-state';
 import { menuState } from '@/game-states/menu-state';
+import { getLevel1 } from '@/game-states/levels/level-1';
+import { getLevel2 } from '@/game-states/levels/level-2';
 
+const debugElement = document.querySelector('#debug')!;
 
 // TESTING
 drawCurrentTexture();
 // END TESTING
 
+createGameStateMachine(gameState, getLevel1);
 
 
-createGameStateMachine(menuState);
+let previousTime = 0;
+const interval = 1000 / 60;
 
-// let lastTime = 0;
 draw(0);
 
-
-function draw(time: number) {
+function draw(currentTime: number) {
   controls.queryController();
+  const delta = currentTime - previousTime;
 
-  getGameStateMachine().getState().onUpdate(time);
+  if (delta >= interval) {
+    previousTime = currentTime - (delta % interval);
+
+    getGameStateMachine().getState().onUpdate(delta);
+  }
+
+  debugElement.textContent = delta.toString();
 
   requestAnimationFrame(draw);
 }
